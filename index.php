ed40af6bbe2ff212f1be6cc0c8a34a756d5c0286
TacoKat <katya.kozub.au@gmail.com>
2026-06-16 12:20:56 +1000

    Setup Commit

--- index.php
+++ index.php
@@ -1,2 +1,26 @@
 <?php
-echo "Holiday Project is running!";
+session_start();
+?>
+<?php include "includes/navbar.php"; ?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <h2>Login</h2>
+            <form method="POST" action="index.php">
+                <input type="email" name="email" placeholder="Email" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
